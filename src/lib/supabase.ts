ad1205dff7ee137164118ae794ac6cfd0c81b822
HAMAD ABDULLAH ISMAIL ALBALOSHI <rock201391@gmail.com>
2026-07-04 20:10:14 +0400

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL ||
+  import.meta.env.SUPABASE_URL;
+
+const supabaseKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY ||
+  import.meta.env.SUPABASE_API_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
